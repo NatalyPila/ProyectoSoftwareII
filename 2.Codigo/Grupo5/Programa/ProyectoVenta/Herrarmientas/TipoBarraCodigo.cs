4493e350f47af47f223846e4c0328d22eda02b0c
geovany <gntoaquiza@espe.edu.ec>
2023-02-19 21:42:51 -0500

    codigo

---
+++ 2.Codigo/Grupo5/Programa/ProyectoVenta/Herrarmientas/TipoBarraCodigo.cs
@@ -0,0 +1,14 @@
+﻿using BarcodeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVenta.Herrarmientas
+{
+    public class TipoBarraCodigo
+    {
+        public static TYPE TipoCodigo = TYPE.CODE128;
+    }
+}
